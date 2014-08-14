d895c282a71f12a6adf3e19d6b6710aa601ba5de
alan hawkins <hawk.alan@gmail.com>
2014-08-15 02:28:36 +1000

    added support for using current directory to infer downstream github repository

--- upstream_everything.sh
+++ upstream_everything.sh
@@ -7,9 +7,9 @@ do
         if git remote | grep 'upstream' > /dev/null; then
             echo "'upstream' remote already exists in ${PWD##*/}"
         else
-            UPSTREAM=`infer-upstream -r "${PWD##*/}" -u silky`
+            UPSTREAM=`infer-upstream --using-cwd`
             if [ $? -eq 0 ]; then
-                git remote add upstream `infer-upstream -r "${PWD##*/}" -u silky`
+                git remote add upstream ${UPSTREAM}
                 echo "created 'upstream' remote in ${PWD##*/} pointing at ${UPSTREAM}"
             else
                 echo "can't determine upstream for ${PWD##*/}"
